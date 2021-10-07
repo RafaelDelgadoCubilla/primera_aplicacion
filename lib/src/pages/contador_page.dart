679923f081bef2a8f2d7e60de16a974351bd8f28
Rafael Delgado Cubilla <rdelcub359@g.educaand.es>
2021-10-07 12:10:15 +0200

    Final Sección 5 con añadidos

--- lib/src/pages/contador_page.dart
+++ lib/src/pages/contador_page.dart
@@ -7,37 +7,113 @@ class ContadorPage extends StatefulWidget {
 
 class _ContadorPageState extends State<ContadorPage> {
   final _miestilo = TextStyle(fontSize: 25.0);
-  int _contador = -50;
+  int _contador = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            _contador++;
-            setState(() {
-              
-            });
-          },
-          child: Icon(Icons.add),
-        ),
+        floatingActionButton: _crearBotones(),
         appBar: AppBar(
           centerTitle: true,
-          title: Text('Texto_APPBAR'),
+          title: SafeArea(child: Text('Texto_APPBAR')),
           //elevation: 20.3,
         ),
         body: Center(
             child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Hola Mundo!!!',
+                  style: _miestilo,
+                ),
+                Text(
+                  '$_contador',
+                  style: _miestilo,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    TextButton(
+                      onPressed: null, 
+                      child: Text('+', style: _miestilo,)
+                    ),
+                    TextButton(
+                      onPressed: null, 
+                      child: Text('+', style: _miestilo,)
+                    ),
+                    TextButton(
+                      onPressed: null, 
+                      child: Text('+', style: _miestilo,)
+                    )
+                  ],
+                )           
+              ] ,
+            )
+          )
+      );
+  }
+
+  Widget _crearBotones() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      crossAxisAlignment: CrossAxisAlignment.end,
+      children: [
+        SizedBox(width: 30.0),
+        Boton3(),
+        Expanded(child: SizedBox()),
+        Boton2(),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.end,
           children: [
-            Text(
-              'Hola Mundo!!!',
-              style: _miestilo,
-            ),
-            Text(
-              '$_contador',
-              style: _miestilo,
-            ),
+            Boton4(),
+            Boton1(),
+            Boton1(),
           ],
-        )));
+        )
+
+      ],
+    );
+  }
+
+  Widget Boton1()
+  {
+    return FloatingActionButton(
+          onPressed: suma,
+          child: Icon(Icons.add),
+        );
+  }
+  Widget Boton2()
+  {
+    return FloatingActionButton(
+          onPressed: resta,
+          child: Icon(Icons.remove),
+        );
+  }
+  Widget Boton3()
+  {
+    return FloatingActionButton(
+          onPressed: cero,
+          child: Icon(Icons.exposure_zero),
+        );
+  }
+  Widget Boton4()
+  {
+    return FloatingActionButton(
+          onPressed: cincuenta,
+          child: Icon(Icons.exposure_zero),
+        );
+  }
+
+  void suma() {
+    setState(() => _contador++);
+  }
+  void resta() {
+    setState(() => _contador--);
+  }
+  void cero() {
+    setState(() => _contador=0);
+  }
+
+  void cincuenta() {
+    setState(() => _contador=50);
   }
 }
